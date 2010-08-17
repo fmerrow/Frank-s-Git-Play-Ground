6b9d6588c9c9f195053fe932521425a1547726ea
Frank Merrow <fmerrow@qualcomm.com>
2010-08-17 07:45:22 -0700

    First Check In . . . only parts of it working

---
+++ FranksColors/FranksColors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QualcommStandardInterfaces;
+
+namespace FranksColors {
+   public class FranksRed : IColorInterface {
+      public FranksRed() { Name = "red"; Description = "This is the color of Frank's Shirt"; }
+      public string Name { get; private set; }
+      public string Description { get; private set; }
+   }
+   public class FranksBlue : IColorInterface {
+      public FranksBlue() { Name = "blue"; Description = "This is the color of the Sky"; }
+      public string Name { get; private set; }
+      public string Description { get; private set; }
+   }
+   public class FranksGreen : IColorInterface {
+      public FranksGreen() { Name = "green"; Description = "This is the color of Trees"; }
+      public string Name { get; private set; }
+      public string Description { get; private set; }
+   }
+}
---
+++ QualcommStandardInterfaces/QualcommStandardInterfaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QualcommStandardInterfaces {
+   // ...
+   // Many Interfaces Defined
+   // ...
+   public interface IColorInterface {
+      string Name { get; }
+      string Description { get; }
+   }
+   // ..
+   // Many More Interfaces Defined
+   // ...
+
+}
